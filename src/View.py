a0d4e696c66c00722979bfab34ffa0fb6fc7d749
Yuuna Takazato <e215724@ie.u-ryukyu.ac.jp>
2022-08-05 01:17:30 +0900

    update: 画面遷移を追加

--- src/View.py
+++ src/View.py
@@ -10,6 +10,8 @@ from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, NumericProperty
+from kivy.uix.popup import Popup
+from kivy.properties import ObjectProperty
 
 ####日本語対応用コード
 from kivy.core.text import LabelBase, DEFAULT_FONT  # 追加分
@@ -19,10 +21,49 @@ LabelBase.register(DEFAULT_FONT, 'Hiragino Sans GB.ttc')  # 追加分
 ####日本語対応ここまで
 
 #ウィンドウサイズの定義
-Config.set('graphics', 'width', 1600)
+Config.set('graphics', 'width', 1200)
 Config.set('graphics', 'height', 800)
 Config.set('graphics', 'resizable', 0)
 
+
+#popupクラス達
+sm=ScreenManager()
+class PopupMenu1(BoxLayout):
+    popup_close = ObjectProperty(None)
+
+class PopupMenu2(BoxLayout):
+    popup_close = ObjectProperty(None)
+    
+class PopupMenu3(BoxLayout):
+    popup_close = ObjectProperty(None)
+
+
+class PopupTest(Screen):
+    def popup_open(self):
+        content = PopupMenu1(popup_close=self.popup_close)
+        self.popup = Popup(title='Popup Test', content=content, size_hint=(0.5, 0.5), auto_dismiss=False)
+        self.popup.open()
+
+class PopupTest(Screen):
+    def popup_open(self):
+        content = PopupMenu2(popup_close=self.popup_close)
+        self.popup = Popup(title='Popup Test', content=content, size_hint=(0.5, 0.5), auto_dismiss=False)
+        self.popup.open()
+
+class PopupTest(Screen):
+    def popup_open(self):
+        content = PopupMenu3(popup_close=self.popup_close)
+        self.popup = Popup(title='Popup Test', content=content, size_hint=(0.5, 0.5), auto_dismiss=False)
+        self.popup.open()
+        
+    def popup_close(self):
+        self.popup.dismiss()
+
+
+class PopupApp(App):
+    def build(self):
+        sm.add_widget(PopupTest(name='popupTest'))
+        return PopupTest()
 #
 # Name: Menu
 # Description: メニュークラス
@@ -43,7 +84,13 @@ class AttendanceScreen(Screen):
 # Description: 課題管理画面クラス
 #
 class TaskManagementScreen(Screen):
-    pass
+    def popup_open(self):
+        content = PopupMenu1(popup_close=self.popup_close)
+        self.popup = Popup(title='Popup Test', content=content, size_hint=(0.5, 0.5), auto_dismiss=False)
+        self.popup.open()
+
+    def popup_close(self):
+        self.popup.dismiss()
 
 #
 # Name: TaskViewScreen
@@ -57,14 +104,26 @@ class TaskViewScreen(Screen):
 # Description: 講義管理画面クラス
 #
 class LectureManagerScreen(Screen):
-    pass
+    def popup_open(self):
+        content = PopupMenu2(popup_close=self.popup_close)
+        self.popup = Popup(title='Popup Test', content=content, size_hint=(0.5, 0.5), auto_dismiss=False)
+        self.popup.open()
+
+    def popup_close(self):
+        self.popup.dismiss()
 
 #
 # Name: AssessmentScreen
 # Description: 見込み評価画面クラス
 #
 class AssessmentScreen(Screen):
-    pass
+    def popup_open(self):
+        content = PopupMenu3(popup_close=self.popup_close)
+        self.popup = Popup(title='Popup Test', content=content, size_hint=(0.5, 0.5), auto_dismiss=False)
+        self.popup.open()
+
+    def popup_close(self):
+        self.popup.dismiss()
 
 #
 # Name: View
@@ -73,7 +132,7 @@ class AssessmentScreen(Screen):
 class View(App):
     def __init__(self, **kwargs):
         super(View, self).__init__(**kwargs)
-        self.title='DENCHU'
+        self.title='StudentReminder'
 
 
     def build(self):
